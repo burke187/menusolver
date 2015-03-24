c59fac843f363c0b9eb3299c8be2a6444e8b58c4
William Burke <burke187@gmail.com>
2015-03-24 14:19:50 -0500

    conditional run

--- run.rb
+++ run.rb
@@ -4,7 +4,11 @@ file = MenuSolver::MenuParser.new ARGV[0] || 'menu.txt'
 
 results = file.parse.solve 
 
-results.each do |result| 
-			p result
+if results == []
+	p "No combo will match target price!"
+	
+else
+	results.each do |result| 
+		p result
+	end
 end
-
