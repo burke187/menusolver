986dd95a1247fdef29797d3981366db761038043
William Burke <burke187@gmail.com>
2015-03-24 14:16:15 -0500

    final

--- run.rb
+++ run.rb
@@ -2,4 +2,9 @@ require_relative "parser"
 
 file = MenuSolver::MenuParser.new ARGV[0] || 'menu.txt'
 
-puts file.parse.solve.inspect
+results = file.parse.solve 
+
+results.each do |result| 
+			p result
+end
+
